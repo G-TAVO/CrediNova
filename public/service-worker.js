63f6ee1c327cde82304be264c3e21deefcfa31c8
Gustavo Enrique Vega Mercado <enriquevega201618@gmail.com>
2026-04-01 22:15:36 -0500

    Update service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,19 +1,27 @@
 self.addEventListener("install", (event) => {
   console.log("Service Worker instalado");
+
   event.waitUntil(
-    caches.open("crediapp-cache").then((cache) => {
+    caches.open("crediapp-cache-v1").then((cache) => {
       return cache.addAll([
-        "/",
-        "/index.html",
-        "/login.html",
-        "/register.html",
-        "/panel.html",
-        "/style.css",
-        "/app.js",
-        "/manifest.json"
+        "./",
+        "./index.html",
+        "./login.html",
+        "./register.html",
+        "./panel.html",
+        "./admin.html",
+        "./style.css",
+        "./app.js",
+        "./manifest.json",
+        "./icons/icon-192.png",
+        "./icons/icon-512.png"
       ]);
     })
   );
+});
+
+self.addEventListener("activate", (event) => {
+  console.log("Service Worker activado");
 });
 
 self.addEventListener("fetch", (event) => {
